b1ac3b14fd59cff2b053ab7728f2ee1291c10d7b
edwin158 <edwinalvarado158@gmail.com>
2015-11-26 22:17:18 -0300

    update settings.py

--- CustomerPortal/settings.py
+++ CustomerPortal/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'CustomerPortal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql_psycopg2'),
-        'USER': 'demosuer',
-        'PASSWORD': '1qaz',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
